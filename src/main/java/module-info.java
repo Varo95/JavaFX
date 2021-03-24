37ba1b53b28503155e26c6165c97467dd832423b
Alvaro <alvaro_beta@hotmail.com>
2021-03-24 18:59:21 +0100

    Funciona SIUUU

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,15 +2,15 @@ module VitalAsistencia {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
-    requires java.activation;
     requires java.sql;
     requires mysql.connector.java;
     requires java.xml.bind;
 
     opens com.Alvaro.controllers to javafx.fxml;
-    opens com.Alvaro.model to java.xml.bind;
-    opens com.Alvaro.utilities to java.xml.bind;
+    opens com.Alvaro.model to java.xml.bind, javafx.baseEmpty;
+    opens com.Alvaro.utilities to java.xml.bind, javafx.baseEmpty;
     exports com.Alvaro;
+    exports com.Alvaro.controllers;
     exports com.Alvaro.model;
     exports com.Alvaro.utilities;
 }
